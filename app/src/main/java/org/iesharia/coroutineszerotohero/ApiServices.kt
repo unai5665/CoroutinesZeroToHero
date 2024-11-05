e6bfd463aaa2271157918cdf0a9fd6ca8db2629d
unai <unaiperez5665@gmail.com>
2024-11-05 13:38:56 +0000

    ApiServices modificado con la nueva API de jsonplaceholder

--- app/src/main/java/org/iesharia/coroutineszerotohero/ApiServices.kt
+++ app/src/main/java/org/iesharia/coroutineszerotohero/ApiServices.kt
@@ -3,69 +3,39 @@ package org.iesharia.coroutineszerotohero
 import com.google.gson.annotations.SerializedName
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Path
 
 interface ApiServices {
 
-    @GET("/api/14c14f8b4a12f4c1cd4098a5ea8092cb/search/{name}")
-    suspend fun getSuperheroes(@Path("name") superheroName: String): Response<SuperHeroDataResponse>
+    @GET("https://jsonplaceholder.typicode.com/users")
+    suspend fun getUsers(): Response<List<UserResponse>>
 }
 
-data class SuperHeroDataResponse(
-    @SerializedName("response") val response: String,
-    @SerializedName("results-for") val resultsFor: String,
-    @SerializedName("results") val results: List<SuperHeroItemResponse>
-)
-
-data class SuperHeroItemResponse(
-    @SerializedName("id") val id: String,
+data class UserResponse(
+    @SerializedName("id") val id: Int,
     @SerializedName("name") val name: String,
-    @SerializedName("powerstats") val powerstats: PowerStats,
-    @SerializedName("biography") val biography: Biography,
-    @SerializedName("appearance") val appearance: Appearance,
-    @SerializedName("work") val work: Work,
-    @SerializedName("connections") val connections: Connections,
-    @SerializedName("image") val image: Image
+    @SerializedName("username") val username: String,
+    @SerializedName("email") val email: String,
+    @SerializedName("address") val address: AddressResponse,
+    @SerializedName("phone") val phone: String,
+    @SerializedName("website") val website: String,
+    @SerializedName("company") val company: CompanyResponse
 )
 
-data class PowerStats(
-    @SerializedName("intelligence") val intelligence: String,
-    @SerializedName("strength") val strength: String,
-    @SerializedName("speed") val speed: String,
-    @SerializedName("durability") val durability: String,
-    @SerializedName("power") val power: String,
-    @SerializedName("combat") val combat: String
+data class AddressResponse(
+    @SerializedName("street") val street: String,
+    @SerializedName("suite") val suite: String,
+    @SerializedName("city") val city: String,
+    @SerializedName("zipcode") val zipcode: String,
+    @SerializedName("geo") val geo: GeoResponse
 )
 
-data class Biography(
-    @SerializedName("full-name") val fullName: String,
-    @SerializedName("alter-egos") val alterEgos: String,
-    @SerializedName("aliases") val aliases: List<String>,
-    @SerializedName("place-of-birth") val placeOfBirth: String,
-    @SerializedName("first-appearance") val firstAppearance: String,
-    @SerializedName("publisher") val publisher: String,
-    @SerializedName("alignment") val alignment: String
+data class GeoResponse(
+    @SerializedName("lat") val lat: String,
+    @SerializedName("lng") val lng: String
 )
 
-data class Appearance(
-    @SerializedName("gender") val gender: String,
-    @SerializedName("race") val race: String,
-    @SerializedName("height") val height: List<String>,
-    @SerializedName("weight") val weight: List<String>,
-    @SerializedName("eye-color") val eyeColor: String,
-    @SerializedName("hair-color") val hairColor: String
-)
-
-data class Work(
-    @SerializedName("occupation") val occupation: String,
-    @SerializedName("base") val base: String
-)
-
-data class Connections(
-    @SerializedName("group-affiliation") val groupAffiliation: String,
-    @SerializedName("relatives") val relatives: String
-)
-
-data class Image(
-    @SerializedName("url") val url: String
+data class CompanyResponse(
+    @SerializedName("name") val name: String,
+    @SerializedName("catchPhrase") val catchPhrase: String,
+    @SerializedName("bs") val bs: String
 )
