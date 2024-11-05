317319139efed2286ace91798ea6ed790485d224
unai <unaiperez5665@gmail.com>
2024-11-05 13:12:32 +0000

    ApiServicesJPH.kt creado

--- app/src/main/java/org/iesharia/coroutineszerotohero/ApiServices.kt
+++ app/src/main/java/org/iesharia/coroutineszerotohero/ApiServices.kt
@@ -7,8 +7,65 @@ import retrofit2.http.Path
 
 interface ApiServices {
 
-    @GET("/api//search({name}")
-    suspend fun getSuperheroes(@Path("name") superheroName:String): Response<SuperHeroDataResponse>
+    @GET("/api/14c14f8b4a12f4c1cd4098a5ea8092cb/search/{name}")
+    suspend fun getSuperheroes(@Path("name") superheroName: String): Response<SuperHeroDataResponse>
 }
 
-data class SuperHeroDataResponse(@SerializedName("response") val response:String)
+data class SuperHeroDataResponse(
+    @SerializedName("response") val response: String,
+    @SerializedName("results-for") val resultsFor: String,
+    @SerializedName("results") val results: List<SuperHeroItemResponse>
+)
+
+data class SuperHeroItemResponse(
+    @SerializedName("id") val id: String,
+    @SerializedName("name") val name: String,
+    @SerializedName("powerstats") val powerstats: PowerStats,
+    @SerializedName("biography") val biography: Biography,
+    @SerializedName("appearance") val appearance: Appearance,
+    @SerializedName("work") val work: Work,
+    @SerializedName("connections") val connections: Connections,
+    @SerializedName("image") val image: Image
+)
+
+data class PowerStats(
+    @SerializedName("intelligence") val intelligence: String,
+    @SerializedName("strength") val strength: String,
+    @SerializedName("speed") val speed: String,
+    @SerializedName("durability") val durability: String,
+    @SerializedName("power") val power: String,
+    @SerializedName("combat") val combat: String
+)
+
+data class Biography(
+    @SerializedName("full-name") val fullName: String,
+    @SerializedName("alter-egos") val alterEgos: String,
+    @SerializedName("aliases") val aliases: List<String>,
+    @SerializedName("place-of-birth") val placeOfBirth: String,
+    @SerializedName("first-appearance") val firstAppearance: String,
+    @SerializedName("publisher") val publisher: String,
+    @SerializedName("alignment") val alignment: String
+)
+
+data class Appearance(
+    @SerializedName("gender") val gender: String,
+    @SerializedName("race") val race: String,
+    @SerializedName("height") val height: List<String>,
+    @SerializedName("weight") val weight: List<String>,
+    @SerializedName("eye-color") val eyeColor: String,
+    @SerializedName("hair-color") val hairColor: String
+)
+
+data class Work(
+    @SerializedName("occupation") val occupation: String,
+    @SerializedName("base") val base: String
+)
+
+data class Connections(
+    @SerializedName("group-affiliation") val groupAffiliation: String,
+    @SerializedName("relatives") val relatives: String
+)
+
+data class Image(
+    @SerializedName("url") val url: String
+)
---
+++ app/src/main/java/org/iesharia/coroutineszerotohero/ApiServicesJPH.kt
@@ -0,0 +1,5 @@
+package org.iesharia.coroutineszerotohero
+
+import com.google.gson.annotations.SerializedName
+import retrofit2.Response
+import retrofit2.http.GET
